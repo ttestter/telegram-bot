add7aeccc874c43ae7f80014b12c8349ccb6cd00
ttestter <ttestter66@gmail.com>
2025-06-03 16:01:47 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$token = "توکن_ربات_تو_اینجا";
+
+$content = file_get_contents("php://input");
+$update = json_decode($content, true);
+
+if(!$update) {
+    exit;
+}
+
+$message = $update["message"];
+$text = $message["text"];
+$chat_id = $message["chat"]["id"];
+
+if($text == "/start") {
+    $reply = "سلام! 👋 ربات روی Railway فعاله.";
+    file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=" . urlencode($reply));
+}
+?>
